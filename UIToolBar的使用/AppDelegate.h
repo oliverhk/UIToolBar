cbe48fb267a39d7de1303daca82bdb505986ef6e
cjq002 <692771080@qq.com>
2015-03-05 08:48:03 +0800

    Initial Commit

---
+++ UIToolBar的使用/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UIToolBar的使用
+//
+//  Created by 陈家庆 on 15-3-5.
+//  Copyright (c) 2015年 shikee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
