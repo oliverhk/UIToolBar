bb84b43039ee4379a29c8256b70ab1ec6d082ab1
cjq002 <692771080@qq.com>
2015-03-05 11:53:34 +0800

    新建工程项目

--- UIToolBar的使用/AppDelegate.m
+++ UIToolBar的使用/AppDelegate.m
@@ -8,12 +8,19 @@
 
 #import "AppDelegate.h"
 
+#import "ViewController.h"
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    
+    ViewController *vc = [[ViewController alloc]init];
+    UINavigationController *navCtr = [[UINavigationController alloc]initWithRootViewController:vc];
+    self.window.rootViewController = navCtr;
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
---
+++ ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UIToolBar的使用
+//
+//  Created by 陈家庆 on 15-3-5.
+//  Copyright (c) 2015年 shikee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
